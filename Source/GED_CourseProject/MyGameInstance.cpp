b4502022637413ca25937d0a80b6ac6c53033774
Emoral12 <eugbermorales77@gmail.com>
2025-10-22 20:52:51 -0400

    Singleton and Factory

--- Source/GED_CourseProject/MyGameInstance.cpp
+++ Source/GED_CourseProject/MyGameInstance.cpp
@@ -1,5 +1,58 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// GameInstance/Singleton for GED_CourseProject, cpp file
 
 
 #include "MyGameInstance.h"
 
+void UMyGameInstance::AddScore(int32 Points)
+{
+	TotalScore += Points;
+
+	// debug text to see if function is even going through or not
+	UE_LOG(LogTemp, Log, TEXT("You got a gem! Your Score is now: %d"), TotalScore);
+}
+
+int32 UMyGameInstance::GetScore() const
+{
+	return TotalScore;
+}
+
+void UMyGameInstance::ResetScore()
+{
+	TotalScore = 0;
+}
+
+void UMyGameInstance::AddHitPoints(int32 HP)
+{
+	PlayerHitPoints += HP;
+
+	// debug text to see if function is even going through or not
+	UE_LOG(LogTemp, Log, TEXT("Your current HP value is: %d"), PlayerHitPoints);
+}
+
+int32 UMyGameInstance::GetHitPoints() const
+{
+	return PlayerHitPoints;
+}
+
+void UMyGameInstance::ResetHitPoints()
+{
+	PlayerHitPoints = 10;
+}
+
+void UMyGameInstance::AddIntensity(int32 Intensity)
+{
+	IntensityValue += Intensity;
+
+	// debug text to see if function is even going through or not
+	UE_LOG(LogTemp, Log, TEXT("Current Intenisty Value: %d"), IntensityValue);
+}
+
+int32 UMyGameInstance::GetIntensity() const
+{
+	return IntensityValue;
+}
+
+void UMyGameInstance::ResetIntensity()
+{
+	IntensityValue = 0;
+}
--- Source/GED_CourseProject/MyGameInstance.h
+++ Source/GED_CourseProject/MyGameInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// GameInstance/Singleton for GED_CourseProject, header file
 
 #pragma once
 
@@ -6,12 +6,56 @@
 #include "Engine/GameInstance.h"
 #include "MyGameInstance.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class GED_COURSEPROJECT_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	// this here adds points to a player score
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	void AddScore(int32 Points);
+
+	// gets the score, so updates it pretty much
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	int32 GetScore() const;
+
+	// resets score, in case of new level but currently not used 
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	void ResetScore();
+
+	// adds or subtracts hp from player
+	UFUNCTION(BlueprintCallable, Category = "Hit Points")
+	void AddHitPoints(int32 HP);
+
+	// gets and updates current hp value 
+	UFUNCTION(BlueprintCallable, Category = "Hit Points")
+	int32 GetHitPoints() const;
+
+	// resets player hp (in the event of a game over or reset)
+	UFUNCTION(BlueprintCallable, Category = "Hit Points")
+	void ResetHitPoints();
+
+	// alters numerical value of intensity 
+	UFUNCTION(BlueprintCallable, Category = "Intensity")
+	void AddIntensity(int32 Intensity);
+
+	// gets and updates current intensity value
+	UFUNCTION(BlueprintCallable, Category = "Intensity")
+	int32 GetIntensity() const;
+
+	// resets intensity value
+	UFUNCTION(BlueprintCallable, Category = "Intensity")
+	void ResetIntensity();
+
+private:
+	// Starting score is set at 0 (duh)
+	int32 TotalScore = 0;
+
+	// HP at the beginning of a playthrough
+	int32 PlayerHitPoints = 10;
+
+	// Sets initial Intenisty value which will shift per events that happen to player
+	int32 IntensityValue = 0;
 };
